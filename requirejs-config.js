25052d7e7df5a57132df67e6fa8370e4bfbafdda
kahnjw <jarrod@yola.com>
2014-04-23 15:04:28 -0700

    Use single quotes instead of double quotes in requirejs config

--- requirejs-config.js
+++ requirejs-config.js
@@ -1,36 +1,36 @@
 var require = {
-  name: "strava-mileage",
-  insertRequire: ["app/strava-mileage"],
-  baseUrl: "app/",
+  name: 'strava-mileage',
+  insertRequire: ['app/strava-mileage'],
+  baseUrl: 'app/',
   map: {
-    "*":{
+    '*':{
       //build-time mapping
-      "underscore": "lodash"
+      'underscore': 'lodash'
     }
   },
   paths: {
-    console: "../bower_components/h5bp-console-polyfill/console",
-    jquery: "../bower_components/jquery/jquery",
-    backbone: "../bower_components/backbone/backbone",
-    lodash: "../bower_components/lodash/dist/lodash",
-    handlebars: "../bower_components/handlebars/handlebars.runtime",
-    bootstrap: "../bower_components/bootstrap/js",
-    "jquery.cookie": "../bower_components/jquery.cookie/jquery.cookie",
-    templates: "../compiled_templates"
+    console: '../bower_components/h5bp-console-polyfill/console',
+    jquery: '../bower_components/jquery/jquery',
+    backbone: '../bower_components/backbone/backbone',
+    lodash: '../bower_components/lodash/dist/lodash',
+    handlebars: '../bower_components/handlebars/handlebars.runtime',
+    bootstrap: '../bower_components/bootstrap/js',
+    'jquery.cookie': '../bower_components/jquery.cookie/jquery.cookie',
+    templates: '../compiled_templates'
   },
   shim: {
-    "jquery.cookie": {
-      deps: ["jquery"]
+    'jquery.cookie': {
+      deps: ['jquery']
     },
-    "bootstrap/bootstrap-transition": {
-      deps: ["jquery"]
+    'bootstrap/bootstrap-transition': {
+      deps: ['jquery']
     },
-    "handlebars": {
-      exports: "Handlebars"
+    'handlebars': {
+      exports: 'Handlebars'
     },
   },
-  out: "build/compiled.js",
+  out: 'build/compiled.js',
   keepBuildDir: true,
-  optimize: "none",
-  optimizeCss: "none"
+  optimize: 'none',
+  optimizeCss: 'none'
 };
