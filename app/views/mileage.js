ef3d795f90991a20866e608a7fbd847b05aaede9
kahnjw <jarrod@yola.com>
2014-05-10 11:18:06 -0700

    Add break points and style meter element

--- app/views/mileage.js
+++ app/views/mileage.js
@@ -4,8 +4,6 @@ var Backbone = require('backbone');
 var mileageTemplate = require('../templates/mileage.rvt');
 var MileageCollection = require('../collections/mileage');
 var rivets = require('rivets');
-var $ = require('jquery');
-var _ = require('lodash');
 
 var Mileage = Backbone.View.extend({
   template: mileageTemplate,
@@ -16,36 +14,16 @@ var Mileage = Backbone.View.extend({
   },
 
   render: function() {
-    this.listenTo(this.collection, 'add', _.bind(this.drawGraphs, this));
-    this.insertView();
-  },
-
-  insertView: function() {
     this.$el.html(this.template);
 
     rivets.bind(this.$el, {
       mileages: this.collection,
       controller: this
     });
-
-    this.drawGraphs();
   },
 
   modelToJson: function(model) {
     return model.toJSON();
-  },
-
-  drawGraphs: function() {
-    _.each(this.collection.models, function(model) {
-      var percent = model.attributes.mileage / model.attributes.lifetime;
-      var $item = $('.' + model.attributes.id);
-
-      if(percent > 1) {
-        model.attributes.over = true;
-        percent = 1;
-      }
-      $item.find('.bar').width((percent * 100 + '%'));
-    });
   }
 });
 
