89d419d350606558326d934a2eeec291e10b4394
kahnjw <jarrod@yola.com>
2014-05-24 11:27:40 -0700

    Fix activities SVG in safari

--- app/views/user-menu.js
+++ app/views/user-menu.js
@@ -2,18 +2,12 @@
 
 var Backbone = require('backbone');
 var userMenuTemplate = require('../templates/user-menu.rvt');
-var activitiesIcon = require('../../images/activities-icon.svg');
-var rivets = require('rivets');
 
 var UserMenu = Backbone.View.extend({
   template: userMenuTemplate,
 
   render: function() {
     this.$el.html(this.template);
-
-    this.rivet = rivets.bind(this.$el, {
-      activitiesIcon: activitiesIcon
-    });
   },
 
   switchActive: function(menuName) {
@@ -22,8 +16,6 @@ var UserMenu = Backbone.View.extend({
   },
 
   clean: function() {
-    this.rivet.unbind();
-    delete this.rivet;
     this.$el.empty();
   }
 });
